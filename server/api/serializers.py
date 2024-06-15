49b0af8de20a6bfdd34ebdb68723444560d42d8b
Michaelkuu <85109674+Michaelkuu@users.noreply.github.com>
2024-06-15 18:35:19 +0200

    Add files via upload

---
+++ server/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import User, Exhibit
+from django.contrib.auth.hashers import make_password # sluzy do hashowania hasla 
+
+class ExhibitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exhibit
+        fields = ['id', 'name', 'description','points']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    visited_exhibits = ExhibitSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'visited_exhibits', 'total_museum_points', 'room_points', 'is_admin']
+        #extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        instance = super().create(validated_data)
+        instance.calculate_total_museum_points()
+        return instance
